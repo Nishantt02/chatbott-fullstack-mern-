50d36cea0c299a40621de61813799945ee9eba24
Nishant chauhan <nishantchauhannn@gmail.com>
2026-01-03 13:36:18 +0530

    Simplify axios instance base URL configuration

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -1,18 +1,10 @@
 
+
 import axios from "axios";
+
 console.log("ENV BASE URL 👉", import.meta.env.VITE_SERVER);
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.VITE_SERVER === "development"
-      ? "http://localhost:5001"
-      : "/",
+  baseURL: import.meta.env.VITE_SERVER,
   withCredentials: true,
 });
-// import axios from "axios";
-
-// console.log("ENV BASE URL 👉", import.meta.env.VITE_SERVER);
-
-// export const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_SERVER,
-//   withCredentials: true,
-// });
